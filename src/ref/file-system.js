f9af7f1de85c1af2981c99e77f100d19b4eae8af
King, Jacob (PLT5889) <jacob.king1@usaa.com>
2019-07-16 17:01:23 -0500

    uncomment file system functions

--- src/ref/file-system.js
+++ src/ref/file-system.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 
 
 // Check if folder exist
-// fs.stat(path.join(__dirname, 'created-folder'), function(error, stats) {
-//     if (error && error.errno === -2) {
-//         // folder doesn't exist
-//     }
-// });
+fs.stat(path.join(__dirname, 'created-folder'), function(error, stats) {
+    if (error && error.errno === -2) {
+        // folder doesn't exist
+    }
+});
 
 
 // Create folder
@@ -20,33 +20,33 @@ fs.mkdir(path.join(__dirname, 'created-folder'), {}, (error) =>{
 
 
 // Create file
-// fs.writeFile(path.join(__dirname, '/created-folder', 'hello-world.txt'), 'Hello World!!', (error) => {
-//     if(error) throw error;
-//     else {
-//         console.log('file created and text written...');
-//     }
-// });
+fs.writeFile(path.join(__dirname, '/created-folder', 'hello-world.txt'), 'Hello World!!', (error) => {
+    if(error) throw error;
+    else {
+        console.log('file created and text written...');
+    }
+});
 
 
 // Append file
-// fs.appendFile(path.join(__dirname, '/created-folder', 'hello-world.txt'), ' I love Node!!', (error) => {
-//     if(error) throw error;
-//     else console.log('text appended...');
-// });
+fs.appendFile(path.join(__dirname, '/created-folder', 'hello-world.txt'), ' I love Node!!', (error) => {
+    if(error) throw error;
+    else console.log('text appended...');
+});
 
 
 // Read File
-// fs.readFile(path.join(__dirname, '/created-folder', 'hello-world.txt'), 'utf8', (error, data) => {
-//     if(error) throw error;
-//     else console.log('Text in file: ', data);
-// });
+fs.readFile(path.join(__dirname, '/created-folder', 'hello-world.txt'), 'utf8', (error, data) => {
+    if(error) throw error;
+    else console.log('Text in file: ', data);
+});
 
 
 // Rename File
-// fs.rename(path.join(__dirname, '/created-folder', 'hello-world.txt'), path.join(__dirname, '/created-folder', 'file-renamed.txt'), (error, data) => {
-//     if(error) throw error;
-//     else console.log('File renamed...');
-// });
+fs.rename(path.join(__dirname, '/created-folder', 'hello-world.txt'), path.join(__dirname, '/created-folder', 'file-renamed.txt'), (error, data) => {
+    if(error) throw error;
+    else console.log('File renamed...');
+});
 
 
     
